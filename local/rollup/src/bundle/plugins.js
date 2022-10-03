83129e55be4097903264c3cd1af9107859ff7169
Rene Haas <king-sora@hotmail.de>
2022-10-03 23:19:26 +0200

    use rollup-plugin-esbuild-resolve

--- local/rollup/src/bundle/plugins.js
+++ local/rollup/src/bundle/plugins.js
@@ -5,6 +5,7 @@ const cssnano = require('cssnano');
 const autoprefixer = require('autoprefixer');
 const { nodeResolve: rollupPluginResolve } = require('@rollup/plugin-node-resolve');
 const { babel: rollupBabelInputPlugin } = require('@rollup/plugin-babel');
+const { esbuildResolve: rollupPluginEsBuildResolve } = require('rollup-plugin-esbuild-resolve');
 const rollupPluginScss = require('rollup-plugin-scss');
 const rollupPluginIgnoreImport = require('rollup-plugin-ignore-import');
 const rollupPluginCommonjs = require('@rollup/plugin-commonjs');
@@ -12,7 +13,6 @@ const rollupPluginAlias = require('@rollup/plugin-alias');
 const rollupPluginTs = require('rollup-plugin-typescript2');
 const rollupPluginLicense = require('rollup-plugin-license');
 const rollupPluginEsBuild = require('../plugins/esbuild');
-const rollupPluginEsBuildResolve = require('../plugins/esbuild-resolve');
 const babelConfigEs5 = require('./babel.config.es5');
 const babelConfigEs6 = require('./babel.config.es2015');
 
