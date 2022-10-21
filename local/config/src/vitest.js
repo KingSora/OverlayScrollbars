7aef902b9bad1175c41231e022e8085cceb30e6f
Rene Haas <king-sora@hotmail.de>
2022-10-21 15:22:18 +0200

    exclude vitest setup file from coverage

--- local/config/src/vitest.js
+++ local/config/src/vitest.js
@@ -1,13 +1,16 @@
 const { resolve } = require('path');
 const { defineConfig } = require('vitest/config');
 
+const setupFile = resolve(__dirname, 'vitest.setup.js');
+
 module.exports = defineConfig({
   test: {
-    setupFiles: resolve(__dirname, 'vitest.setup.js'),
+    setupFiles: setupFile,
     environment: 'jsdom',
     include: ['test/**/*.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     coverage: {
       reportsDirectory: './.coverage/unit',
+      exclude: [setupFile],
     },
   },
 });
