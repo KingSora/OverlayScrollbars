2910ab0a5c29fe2b39b5d223d5224f8c2d6ad714
Rene Haas <king-sora@hotmail.de>
2023-07-09 23:12:28 +0200

    update docs

---
+++ website/next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+}
