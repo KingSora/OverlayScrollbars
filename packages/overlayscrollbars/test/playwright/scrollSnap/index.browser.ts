6379a0370145c17aa23f86b058fb3fb04f032535
Rene Haas <king-sora@hotmail.de>
2024-05-08 22:04:23 +0200

    improve snap scroll tests

--- packages/overlayscrollbars/test/playwright/scrollSnap/index.browser.ts
+++ packages/overlayscrollbars/test/playwright/scrollSnap/index.browser.ts
@@ -8,7 +8,9 @@ OverlayScrollbars.plugin(ClickScrollPlugin);
 
 const startBtn = document.querySelector<HTMLButtonElement>('#start')!;
 const targetElm = document.querySelector<HTMLElement>('#target')!;
+const targetRTLElm = document.querySelector<HTMLElement>('#targetRTL')!;
 const viewportElement = document.querySelector<HTMLElement>('#viewport')!;
+const viewportRTLElement = document.querySelector<HTMLElement>('#viewportRTL')!;
 // @ts-ignore
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const osInstance = (window.os = OverlayScrollbars(
@@ -16,6 +18,21 @@ const osInstance = (window.os = OverlayScrollbars(
     target: targetElm,
     elements: {
       viewport: viewportElement,
+    },
+  },
+  {
+    scrollbars: {
+      clickScroll: true,
+    },
+  }
+));
+// @ts-ignore
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const osInstanceRTL = (window.osRTL = OverlayScrollbars(
+  {
+    target: targetRTLElm,
+    elements: {
+      viewport: viewportRTLElement,
     },
   },
   {
--- packages/overlayscrollbars/test/playwright/scrollSnap/index.test.ts
+++ packages/overlayscrollbars/test/playwright/scrollSnap/index.test.ts
@@ -1,17 +1,10 @@
-/*
 import { playwrightRollup, expectSuccess } from '@~local/playwright-tooling';
 import { test } from '@playwright/test';
 
 playwrightRollup();
 
-test.describe('appear', () => {
+test.describe('scrollSnap', () => {
   test('default', async ({ page }) => {
     await expectSuccess(page);
   });
-
-  test('with autoHideSuspend', async ({ page }) => {
-    await page.click('#autoHideSuspend');
-    await expectSuccess(page);
-  });
 });
-*/
